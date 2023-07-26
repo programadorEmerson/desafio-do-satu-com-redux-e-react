bc2a59210556a41b5f67d8642e8e4ad456558019
Emerson Saturnino <programador.emerson@hotmail.com>
2023-07-26 16:06:16 -0300

    copia aula 2 concluída

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
+
+import Header from '@/components/Header'
+
 import reactLogo from './assets/react.svg'
+
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -9,20 +14,27 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <Header />
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is 
+          {' '}
+          {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit 
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/enums': path.resolve(__dirname, 'src/enums'),
+      '@/hooks': path.resolve(__dirname, 'src/hooks'),
+      '@/interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '@/pages': path.resolve(__dirname, 'src/pages'),
+      '@/schemas': path.resolve(__dirname, 'src/schemas'),
+      '@/styles': path.resolve(__dirname, 'src/styles'),
+      '@/utils': path.resolve(__dirname, 'src/utils'),
+    }
+  },
 })
